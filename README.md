b4fdcf0809f05b88d7d16c80d2a4eedf6a4f7ddc
ricardo-miotto-redin <ricardo.miotto-redin@hp.com>
2021-08-01 13:15:06 -0300

    Small tweaks

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # collatz
-A Brute Force check Collatz Conjecture
+An arbitrary-precision Brute-force checker for [Collatz Conjecture](https://en.wikipedia.org/wiki/Collatz_conjecture).
+
+## COMPILE
+`javac com/conjecture/Collatz.java`
+- Requires Java 8 or above.
+
+## RUN
+`java -cp . com.conjecture.Collatz 295147905179352825856`
+- First program argument is the number to start from with default of 5.
+- This will run forever outputting checks of each positive Integer number. 
+- It will only stop and exit if it ever finds a loop above 5.
+
+## Limitations
+- It uses as many threads as the number of cores seen by the running JVM
--- com/conjecture/Collatz.java
+++ com/conjecture/Collatz.java
@@ -11,28 +11,27 @@ public class Collatz {
     private static final BigInteger TWO = new BigInteger("2");
     private static final BigInteger THREE = new BigInteger("3");
     private static BigInteger MIN = THREE.add(TWO);
+    private static final int threads = Runtime.getRuntime().availableProcessors()*2;
     private static final ThreadPoolExecutor executor =
-            (ThreadPoolExecutor) Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()-1);
-    private static  final Object lock = new Object();
-    private static final int size = Runtime.getRuntime().availableProcessors()-1;
-    private static final BlockingQueue<StringBuilder> q = new ArrayBlockingQueue<>(size);
+            (ThreadPoolExecutor) Executors.newFixedThreadPool(threads);
+    private static final Object lock = new Object();
+    private static final BlockingQueue<StringBuffer> q = new ArrayBlockingQueue<>(threads);
 
     public static void main(String[] args) throws InterruptedException {
         BigInteger i = MIN;
         if(args != null && args.length == 1 ) {
             i = new BigInteger(args[0]);
         }
-        for (int j = 0; j < size; j++) {
+        for (int j = 0; j < threads; j++) {
             try {
-                q.put(new StringBuilder());
+                q.put(new StringBuffer());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         executor.prestartAllCoreThreads();
         do {
-            StringBuilder token = q.take();
-            System.out.println(token);
+            System.out.println(q.take());
             Runnable r = new SingleCollatz(i);
             executor.submit(r);
             i = i.add(BigInteger.ONE);
@@ -49,11 +48,11 @@ public class Collatz {
 
     private static class SingleCollatz implements Runnable{
         BigInteger c;
+        StringBuffer s = new StringBuffer();
         public SingleCollatz(BigInteger p) {
             this.c = p;
         }
         public void run() {
-            StringBuilder s = new StringBuilder();
             s.append(c);
             BigInteger i = this.c;
             List<BigInteger> END = Collections.synchronizedList(new ArrayList<>());
